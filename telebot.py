1905cfc58eb688c06c04f8b3377e34f7b1eee9e1
Naki <alicia.neko@mail.ru>
2026-04-19 05:46:17 -0400

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -30,6 +30,7 @@ from botcmd.manga import MangaCommands
 from botcmd.adult_manga import AdultMangaCommands
 from botcmd.tdl import TorrentDownloadCommands
 from botcmd.files import FileCommands
+from botcmd.utils import safe_call
 
 set_cmd = False
 user_settings = {}
@@ -139,17 +140,6 @@ async def add_cover_art(mp3_path: str, cover_url: str) -> None:
     finally:
         if os.path.exists(temp_cover_path):
             os.remove(temp_cover_path)
-            
-async def safe_call(func, *args, **kwargs):
-    while True:
-        try:
-            return await func(*args, **kwargs)
-        except FloodWait as e:
-            print(f"⏳ Esperando {e.value} seg para continuar")
-            await asyncio.sleep(e.value)
-        except Exception as e:
-            print(f"❌ Error inesperado en {func.__name__}: {type(e).__name__}: {e}")
-            raise
 
 def format_time(seconds):
     m, s = divmod(seconds, 60)
